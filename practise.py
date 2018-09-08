b90e0f8c6f15b80e8669013779bfa43bce8c2621
Md Alam <alamwebtech@gmail.com>
2018-09-08 19:34:01 -0400

    trutle squire

--- practise.py
+++ practise.py
@@ -9,3 +9,10 @@ my_turtle.left(90)
 my_turtle.forward(100)
 my_turtle.right(50)
 
+
+
+my_turtle=turtle.Turtle()
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(100)
+my_turtle.right(50)
