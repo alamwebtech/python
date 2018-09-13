c0507be6890fee3c82911ecb6fbb0929709382df
Md Alam <alamwebtech@gmail.com>
2018-09-13 11:44:35 -0400

    learning python

---
+++ myturtle.py
@@ -0,0 +1,30 @@
+# Make a square with the turtle.
+
+import turtle 
+
+
+my_turtle = turtle.Turtle()
+my_turtle.forward(100)
+my_turtle = turtle.Turtle()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+my_turtle = turtle.Turtle()
+my_turtle = turtle.forward(100)
